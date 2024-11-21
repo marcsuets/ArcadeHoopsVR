9e427a726ab102492b93cab0ab5dd3a8f1e84825
Jan <janclararobles@gmail.com>
2024-11-21 12:40:43 +0100

    Update

--- Assets/Tomerinio/LEDLightBlocks/Contents/Scripts/LEDNode.cs
+++ Assets/Tomerinio/LEDLightBlocks/Contents/Scripts/LEDNode.cs
@@ -15,21 +15,21 @@ public class LEDNode : MonoBehaviour
     // Slow light progress indicator.
     public bool slowProgress = false;
 
-
     // Attached components.
     private Light pointLight = null;
     private Renderer rend = null;
 
     // Emitted light intensity parameters.
-    private float intensity = 0;
+    private float intensity = 10.0f; // Fixed intensity for constant light
     private float minIntensity = 0;
-    private float maxIntensity = 20.0f;
+    private float maxIntensity = 20.0f; // max intensity is unused now
     private float onTime = 0;
-    private float maxOnTime = 1.0f;
-    private float onTimerSpeed = 5.0f;
+    private float maxOnTime = 1.0f; // max on time is also unused
+    private float onTimerSpeed = 5.0f; // this is not necessary anymore
     private float offTime = 0;
-    private float maxOffTime = 20.0f;
-    private float offTimerSpeed = 20.0f;
+    private float maxOffTime = 20.0f; // max off time is also unused
+    private float offTimerSpeed = 20.0f; // this is not necessary anymore
+
     // Emitted light increase/decrease controls.
     private enum LightState { INCR, DECR, IDLE }
     private LightState lightState = LightState.IDLE;
@@ -39,129 +39,39 @@ public class LEDNode : MonoBehaviour
         // Init the attached components.
         pointLight = this.GetComponent<Light>();
         rend = GetComponent<Renderer>();
+
+        // Ensure the point light is enabled if desired
+        if (isPointLightEn)
+        {
+            pointLight.enabled = true;  // Make sure the light is ON from the start
+        }
+
+        // Set the intensity to a constant value (maximum)
+        intensity = maxIntensity;
+
+        // Make sure the material color is updated to reflect the intensity immediately
+        UpdateColor();
     }
 
     void Update()
     {
-        ResolveNodeState();
+        // No need to resolve state since we want the light to remain constant and always on
         UpdateColor();
     }
 
-    // Decides on state of fading in/out based on the input parameters.
-    private void ResolveNodeState()
-    {
-        switch (lightState)
-        {
-            case LightState.INCR:
-                IntensityIncrease();
-                break;
-            case LightState.DECR:
-                IntensityDecrease();
-                break;
-            case LightState.IDLE:
-                LightIdle();
-                break;
-            default:
-                break;
-        }
-    }
-
-    // Gradually increase intensity level up to the maximum-level defined in GUI.
-    private void IntensityIncrease()
-    {
-        if (!slowProgress)
-        {
-            if (nextStart == false)
-                nextStart = true;
-        }
-
-        intensity = maxIntensity;
-
-        // If point light is enabled, light it up.
-        if (isPointLightEn)
-            pointLight.enabled = true;
-
-        // If ON timer handn't been reached yet
-        if (onTime < maxOnTime)
-            onTime += onTimerSpeed * Time.deltaTime;
-        // ON timer had been reached.
-        else
-        {
-            onTime = 0;
-            // Start decreasing intensity.
-            lightState = LightState.DECR;
-        }
-    }
-
-    // Gradually decrease intensity level down to 0.
-    private void IntensityDecrease()
-    {
-        intensity = minIntensity;
-
-        // If there's a point light component enabled, disable the point light.
-        if (isPointLightEn)
-            pointLight.enabled = false;
-
-        if (slowProgress)
-        {
-            if (nextStart == false)
-                nextStart = true;
-        }
-        else
-        {
-            // Stop sending the start signal to the next node.
-            if (nextStart == true)
-                nextStart = false;
-        }
-
-        // Move to the idle (no light) state.
-        lightState = LightState.IDLE;
-    }
-
-    // Light idles until signaled to do otherwise.
-    private void LightIdle()
-    {
-        if (slowProgress)
-        {
-            if (nextStart == true)
-                nextStart = false;
-        }
-
-        // If there is a previous linked node
-        if (prevNode != null)
-        {
-            if (prevNode.nextStart)
-            {
-                // Start increasing light intensity.
-                lightState = LightState.INCR;
-            }
-        }
-        // If first node in the chain
-        else if (isFirstNode)
-        {
-            // If max Idle time wasn't reached yet
-            if (offTime < maxOffTime)
-                offTime += offTimerSpeed * Time.deltaTime;
-            else
-            {
-                offTime = 0;
-                // Start increasing light intensity.
-                lightState = LightState.INCR;
-                // Light-up the next node in chain.
-                if (nextStart == false)
-                    nextStart = true;
-            }
-        }
-    }
-
-    // Updates the calculated LED color and intensity level.
+    // Update color for constant phosphorescent light
     private void UpdateColor()
     {
         Material mat = rend.material;
-        Color baseColor = mat.color;
 
-        // Calculate the resulting color based on the intensity.
-        Color finalColor = baseColor * Mathf.LinearToGammaSpace(intensity);
+        // Set a bright neon green color for the phosphorescent effect
+        Color finalColor = new Color(0.0f, 1.0f, 0.0f) * Mathf.LinearToGammaSpace(intensity);
         mat.SetColor("_EmissionColor", finalColor);
+
+        // Ensure the point light is using the same color
+        if (pointLight != null && pointLight.enabled)
+        {
+            pointLight.color = new Color(0.0f, 1.0f, 0.0f); // Neon green
+        }
     }
 }
